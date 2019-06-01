81966e6670cfaa779e28c570136ad1bda52756cc
Svxxk776789k <49727508+Svxxk776789k@users.noreply.github.com>
2019-06-01 15:03:29 +0200

    Update ftr.js

--- BetterAlis/res/ftr.js
+++ BetterAlis/res/ftr.js
@@ -54,7 +54,8 @@ player.hat = btaHatVal.value
 if(user.numSides){
 if(player.uid == user.uid)player.numSides=user.numSides
 } else {
-  if(player.pid==window.playerid)player.numSides=btaWalls.value
+  if(btaWalls.value == 1){if(player.pid==window.playerid)player.numSides=0}else{
+  if(player.pid==window.playerid)player.numSides=btaWalls.value}
 }
 
 if(user.forceSkin){
