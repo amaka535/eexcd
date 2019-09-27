b9343b6e20f7b656f56bf37c8657da06fc72b634
Svxxk776789k <49727508+Svxxk776789k@users.noreply.github.com>
2019-09-27 19:09:57 +0200

    Update multibox.js

--- Hypertion/multibox.js
+++ Hypertion/multibox.js
@@ -1,4 +1,5 @@
-
+var iframeBody=$('#multiboxtab').contents().find('body');
+var iframe = $("#multiboxtab")[0]
 
 if(localStorage.getItem("hypertionID") == 69){ //testing multiboxing
   $("body").append(`<iframe frameBorder="0" id="multiboxtab" src="https://dev.vanis.io/" style="display:none;x-index:99999;top:0;left:0;position: absolute;width:100%;height:100%;"></iframe>`)
@@ -8,8 +9,6 @@ setTimeout(function(){
           objEvent.preventDefault(); // stops its action
       }
   })
-  var iframeBody=$('#multiboxtab').contents().find('body');
-  var iframe = $("#multiboxtab")
   $(iframeBody).on('keyup',function(e) {
     if (e.which == 9) {
       e.preventDefault();
