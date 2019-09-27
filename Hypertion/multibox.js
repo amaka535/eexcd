8d1c7d8f6758438e5221cac7f7b914e435f1bf97
Svxxk776789k <49727508+Svxxk776789k@users.noreply.github.com>
2019-09-27 18:22:22 +0200

    Add files via upload

---
+++ Hypertion/multibox.js
@@ -0,0 +1,31 @@
+var multiboxTab = true
+function imMultiboxTab(){
+  multiboxTab = false
+  $(document).keydown(function(objEvent) {
+      if (objEvent.keyCode == 9) {  //tab pressed
+          objEvent.preventDefault(); // stops its action
+      }
+  })
+}
+
+if(localStorage.getItem("hypertionID") == 69 && multiboxTab){ //testing multiboxing
+  $("body").append(`<iframe frameBorder="0" id="multiboxtab" src="https://dev.vanis.io/" style="display:none;x-index:99999;top:0;left:0;position: absolute;width:100%;height:100%;"></iframe>`)
+  $(document).keydown(function(objEvent) {
+      if (objEvent.keyCode == 9) {  //tab pressed
+          objEvent.preventDefault(); // stops its action
+      }
+  })
+  document.getElementById('multiboxtab').contentWindow.imMultiboxTab();
+  document.onkeyup = function(e) {
+    if (e.which == 9) {
+      e.preventDefault();
+      if($("#multiboxtab").css("display") === "none"){
+        $("#multiboxtab").fadeIn(140)
+        $("#multiboxtab").trigger("focus")
+      } else {
+        $("body").trigger("focus")
+        $("#multiboxtab").fadeOut(140)
+      }
+    }
+  };
+}
