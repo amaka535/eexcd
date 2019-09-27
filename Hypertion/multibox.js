9a213056f0977123704ded065f6332b99b9deaab
Svxxk776789k <49727508+Svxxk776789k@users.noreply.github.com>
2019-09-27 18:29:10 +0200

    Update multibox.js

--- Hypertion/multibox.js
+++ Hypertion/multibox.js
@@ -6,11 +6,20 @@ function imMultiboxTab(){
           objEvent.preventDefault(); // stops its action
       }
   })
+  document.onkeyup = function(e) {
+    if (e.which == 9) {
+      e.preventDefault();
+        $("body").trigger("focus")
+        $("#multiboxtab").fadeOut(140)
+      }
+    }
+  };
 }
 
 if(localStorage.getItem("hypertionID") == 69 && multiboxTab){ //testing multiboxing
   $("body").append(`<iframe frameBorder="0" id="multiboxtab" src="https://dev.vanis.io/" style="display:none;x-index:99999;top:0;left:0;position: absolute;width:100%;height:100%;"></iframe>`)
 setTimeout(function(){
+  if(multiboxTab){
   $(document).keydown(function(objEvent) {
       if (objEvent.keyCode == 9) {  //tab pressed
           objEvent.preventDefault(); // stops its action
@@ -28,6 +37,6 @@ setTimeout(function(){
         $("#multiboxtab").fadeOut(140)
       }
     }
-  };
+  };}
 },2000)
 }
