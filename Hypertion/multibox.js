d5655d45e145e264059a09c9ad07059b0e5eb1a9
Svxxk776789k <49727508+Svxxk776789k@users.noreply.github.com>
2019-09-27 19:13:49 +0200

    Update multibox.js

--- Hypertion/multibox.js
+++ Hypertion/multibox.js
@@ -1,23 +1,22 @@
-var iframeBody=$('#multiboxtab').contents().find('body');
-var iframe = $("#multiboxtab")[0]
-
 if(localStorage.getItem("hypertionID") == 69){ //testing multiboxing
   $("body").append(`<iframe frameBorder="0" id="multiboxtab" src="https://dev.vanis.io/" style="display:none;x-index:99999;top:0;left:0;position: absolute;width:100%;height:100%;"></iframe>`)
 setTimeout(function(){
+  var iframeBody=$('#multiboxtab').contents().find('body');
+  var iframe = $("#multiboxtab")[0]
   $(document).keydown(function(objEvent) {
       if (objEvent.keyCode == 9) {  //tab pressed
           objEvent.preventDefault(); // stops its action
       }
   })
-  $(iframeBody).on('keyup',function(e) {
+  $('#multiboxtab').contents().find('body').on('keyup',function(e) {
     if (e.which == 9) {
       e.preventDefault();
       if($("#multiboxtab").css("display") === "none"){
         document.activeElement.blur();
         $("#multiboxtab").show()
-        iframe.contentWindow.focus();
-      } else {
-        iframe.activeElement.blur();
+document.getElementsByName("multiboxtab")[0].contentWindow.document.body.focus();
+} else {
+document.getElementsByName("multiboxtab")[0].contentWindow.document.activeElement.blur();
         $("#multiboxtab").hide()
         window.focus();
       }
